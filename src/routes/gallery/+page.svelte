0689544ed5280744206a951f85913fd9587e84c5
Jérémy Girard <75862623+Jemery-Dev@users.noreply.github.com>
2024-05-22 21:57:02 +0200

    Update fichier

---
+++ src/routes/gallery/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+    import games from '$lib/games.json';
+    import locations from '$lib/locations.json';
+    //import items from '$lib/items.json';
+    //import characters from '$lib/characters.json';
+    import types from '$lib/types.json';
+</script>
+
+{#each games as game}
+    <div class="game-section">
+        <h1>{game.title}</h1>
+
+        {#each types as type}
+            <h2>{type.type}</h2>
+
+            {#if type.type === "Locations"}
+                <div class="section-item">
+                    {#each locations as location}
+                        {#if location.game === game.title}
+                            <div class="card-item">
+                                <h3>{location.location_name}</h3>
+                                <img alt="{location.location_name}" src="{location.location_src}" />
+                            </div>
+                        {/if}
+                    {/each}
+                </div>
+            {/if}
+
+            {#if type.type === "Items"}
+            {/if}
+
+            {#if type.type === "Characters"}
+            {/if}
+
+            {#if type.type === "Dungeons maps"}
+            {/if}
+        {/each}
+    </div>
+{/each}
+
+<style>
+    .game-section {
+        margin-bottom: 2em;
+    }
+    .card-item {
+        margin-left: 1em;
+    }
+
+    .section-item{
+        display: flex;
+    }
+</style>
