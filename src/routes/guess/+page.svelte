e8bab6801feccf48cae009c5b03587b64bab62c3
Jérémy Girard <75862623+Jemery-Dev@users.noreply.github.com>
2024-05-27 23:44:54 +0200

    azdzad

--- src/routes/guess/+page.svelte
+++ src/routes/guess/+page.svelte
@@ -17,6 +17,11 @@
     let selectedLocation = '';
     let isCorrect = null;
     let gameList = [];
+    let gameStarted = false;
+    let startTime;
+    let endTime;
+    let totalTime;
+    let gameEnded = false;
 
     function getCategoryList(type) {
         switch (type.toLowerCase()) {
@@ -66,59 +71,79 @@
 
         attempts++;
 
-        if(gameList.length < 1 ){
+        if (gameList.length < 1) {
             stopGame();
+        } else {
+            selectRandomLocation();
         }
     }
 
-
-    function stopGame(){
-        
+    function stopGame() {
+        endTime = new Date();
+        totalTime = Math.round((endTime - startTime) / 1000);
+        gameEnded = true;
     }
+
     onMount(function () {
         const categoryList = getCategoryList(selectedType);
         gameList = initializeLocations(categoryList);
         if (gameList.length > 0) {
             selectRandomLocation();
+            startTime = new Date(); // Start the timer when the game starts
+            gameStarted = true;
         }
     });
 </script>
 
-<div class="content-center items-center justify-center">
-    <h1>You're looking for {selectedType} in {selectedGame}</h1>
-
-    {#if currentLocation}
-        <div class="text-4xl font-black">
-            <h1>Guess the {selectedType.charAt(0).toUpperCase() + selectedType.slice(1)}</h1>
-        </div>
-        <div class="flex justify-center items-center">
-            <img src={currentLocation.src} alt={selectedType}/>
-        </div>
-
-        <div class="flex items-center justify-center">
-            <form on:submit|preventDefault={verifyAnswer}>
-                <label>Choose a {selectedType} from this list:
-                    <input list="locationList" name="locationInput" bind:value={selectedLocation}/>
-                </label>
-                <datalist id="locationList">
-                    {#each gameList as question}
-                        <option value={question.name.toLowerCase()}>{question.name}</option>
-                    {/each}
-                </datalist>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-
-        {#if isCorrect === true}
-            <p>Correct! You have earned 1 point. The correct answer was indeed {currentLocation.name}.</p>
-        {:else if isCorrect === false}
-            <p>Incorrect !.</p>
-        {/if}
-
-        <p>Score: {gameScore} || {selectedType.charAt(0).toUpperCase() + selectedType.slice(1)} remaining: {gameList.length}</p>
+{#if gameEnded}
+    <div class="content-center items-center justify-center">
+        <h1>Game Over</h1>
+        <p>Your final score: {gameScore}</p>
         <p>Total attempts: {totalAttempts}</p>
-    {/if}
-</div>
+        <p>Total time taken: {totalTime} seconds</p>
+
+        <button href="">Retry ?</button>
+        <button href="">Home</button>
+
+    </div>
+{:else}
+    <div class="content-center items-center justify-center">
+        <h1>You're looking for {selectedType} in {selectedGame}</h1>
+
+        {#if currentLocation}
+            <div class="text-4xl font-black">
+                <h1>Guess the {selectedType.charAt(0).toUpperCase() + selectedType.slice(1)}</h1>
+            </div>
+            <div class="flex justify-center items-center">
+                <img src={currentLocation.src} alt={selectedType}/>
+            </div>
+
+            <div class="flex items-center justify-center">
+                <form on:submit|preventDefault={verifyAnswer}>
+                    <label>Choose a {selectedType} from this list:
+                        <input list="locationList" name="locationInput" bind:value={selectedLocation}/>
+                    </label>
+                    <datalist id="locationList">
+                        {#each gameList as question}
+                            <option value={question.name.toLowerCase()}>{question.name}</option>
+                        {/each}
+                    </datalist>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+
+            {#if isCorrect === true}
+                <p>Correct! You have earned 1 point. The correct answer was indeed {currentLocation.name}.</p>
+            {:else if isCorrect === false}
+                <p>Incorrect! The correct answer was {currentLocation.name}.</p>
+            {/if}
+
+            <p>Score: {gameScore} || {selectedType.charAt(0).toUpperCase() + selectedType.slice(1)}
+                remaining: {gameList.length}</p>
+            <p>Total attempts: {totalAttempts}</p>
+        {/if}
+    </div>
+{/if}
 
 <style>
     @import 'styles.css';
